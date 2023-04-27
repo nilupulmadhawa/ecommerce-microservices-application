f3f8792238eb8cd230d22b99c58912a9c05694d1
SandalikaAriyrathna <dilsandu96@gmail.com>
2023-04-27 19:23:56 +0530

    slider

--- client/src/components/MainPage/Sdata.js
+++ client/src/components/MainPage/Sdata.js
@@ -1,27 +1,27 @@
 const Sdata = [
   {
     id: 1,
-    title: "50% Off For Your First Shopping",
-    desc: "Get your medical products today.",
-    cover: "./images/SlideCard/23.jpg",
+    title: '50% Off For Your First Shopping',
+    desc: 'Get your medical products today.',
+    cover: './images/SlideCard/23.jpg',
   },
   {
     id: 2,
-    title: "50% Off For Your First Shopping",
-    desc: "Get your medical products today.",
-    cover: "./images/SlideCard/slide-2.png",
+    title: '50% Off For Your First Shopping',
+    desc: 'Get your medical products today.',
+    cover: './images/SlideCard/product_8.jpg',
   },
   {
     id: 3,
-    title: "50% Off For Your First Shopping",
-    desc: "Get your medical products today.",
-    cover: "./images/SlideCard/slide-3.png",
+    title: '50% Off For Your First Shopping',
+    desc: 'Get your medical products today.',
+    cover: './images/SlideCard/product_6.jpg',
   },
   {
     id: 4,
-    title: "50% Off For Your First Shopping",
-    desc: "Get your medical products today.",
-    cover: "./images/SlideCard/slide-4.png",
+    title: '50% Off For Your First Shopping',
+    desc: 'Get your medical products today.',
+    cover: './images/SlideCard/product_7.jpg',
   },
-]
-export default Sdata
+];
+export default Sdata;
