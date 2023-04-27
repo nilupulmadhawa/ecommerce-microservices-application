cda492fe45ba1af969bc6e82ce2834f15154ef22
Nilupul Madhawa <dnmadawa@gmail.com>
2023-04-27 22:16:53 +0530

    update

--- client/src/common/header/Search.jsx
+++ client/src/common/header/Search.jsx
@@ -8,7 +8,7 @@ const Search = ({ CartItem }) => {
   // fixed Header
   window.addEventListener('scroll', function () {
     const search = document.querySelector('.search');
-    search.classList.toggle('active', window.scrollY > 100);
+    // search.classList.toggle('active', window.scrollY > 100);
   });
 
   return (
