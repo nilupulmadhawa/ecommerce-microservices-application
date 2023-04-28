4c6b9eb08d8074e2e4f1fd0ddde7d1636e92ed92
Nilupul Madhawa <dnmadawa@gmail.com>
2023-04-29 01:32:37 +0530

    Update account.js

--- client/src/_mock/account.js
+++ client/src/_mock/account.js
@@ -4,7 +4,7 @@ const account = {
     displayName: 'Mihiran Jayasekara',
     email: 'demo@minimals.cc',
     photoURL: '/assets/images/avatars/avatar_default.jpg',
-    role: 'admin'
+    role: 'Seller'
 };
 
 export default account;
