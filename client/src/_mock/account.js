7d218b5c1633999bfb5423aa6708bd7dc7b1220c
Nilupul Madhawa <dnmadawa@gmail.com>
2023-04-29 01:26:44 +0530

    fixd

--- client/src/_mock/account.js
+++ client/src/_mock/account.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 
 const account = {
-    displayName: 'Admin',
+    displayName: 'Mihiran Jayasekara',
     email: 'demo@minimals.cc',
     photoURL: '/assets/images/avatars/avatar_default.jpg',
     role: 'admin'
